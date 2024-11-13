2a653d33dd068ca64474972ec6e377f97da9c99a
Anna Frolova <an.frolova023@gmail.com>
2024-11-13 15:40:13 +0700

    KS-2-3 train arrival calculation

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,16 @@
+package lesson_2
+
+fun main() {
+
+    val departureHours = 9
+    val departureMinutes = 39
+    val travelTime = 457
+
+    val departureTimeInMinutes = departureHours * 60 + departureMinutes
+    val arrivalTimeInMinutes = departureTimeInMinutes + travelTime
+
+    val arrivalHours = arrivalTimeInMinutes / 60
+    val arrivalMinutes = arrivalTimeInMinutes - arrivalHours * 60
+
+    println("Время прибытия поезда: $arrivalHours:$arrivalMinutes")
+}
