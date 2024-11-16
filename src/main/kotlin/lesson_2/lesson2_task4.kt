9d9c5e7cf89a13e82c58922de4e51cb305c2bfb2
Anna Frolova <an.frolova023@gmail.com>
2024-11-16 12:54:51 +0700

    KS-2-4 add constant

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -6,10 +6,12 @@ fun main() {
     val ironOre = 11
     val buffInPercentage = 20
 
-    val buffForCalculations = buffInPercentage * 0.01
+    val buffForCalculations = buffInPercentage * AS_DECIMAL_FRACTION
     val crystalOreBonus = (crystalOre * buffForCalculations).toInt()
     val ironOreBonus = (ironOre * buffForCalculations).toInt()
 
     println("кристаллическая руда +$crystalOreBonus")
     println("железная руда +$ironOreBonus")
 }
+
+const val AS_DECIMAL_FRACTION = 0.01
