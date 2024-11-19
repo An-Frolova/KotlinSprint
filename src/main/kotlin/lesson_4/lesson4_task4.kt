1dca7e025de8d0b63020a6addf57f24cdd1727c5
Anna Frolova <an.frolova023@gmail.com>
2024-11-19 22:30:16 +0700

    KS-4-4 refactor

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,11 +3,14 @@ package lesson_4
 fun main() {
 
     val day = 5
+    val isEvenDay = day % 2 == 0
 
-    println("""
-        Упражнения для рук:    ${day%2==1}
-        Упражнения для ног:    ${day%2==0}
-        Упражнения для спины:  ${day%2==0}
-        Упражнения для пресса: ${day%2==1}
-    """.trimIndent())
+    println(
+        """
+        Упражнения для рук:    ${!isEvenDay}
+        Упражнения для ног:    $isEvenDay
+        Упражнения для спины:  $isEvenDay
+        Упражнения для пресса: ${!isEvenDay}
+    """.trimIndent()
+    )
 }
