59042a335365df0e31b5732b777992d74aaff4b8
Anna Frolova <an.frolova023@gmail.com>
2024-11-19 12:12:34 +0700

    KS-4-2 refactor

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -7,18 +7,17 @@ fun main() {
     val weight2 = 50
     val volume2 = 100
 
-
     println(
         "Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average':" +
-            "${weight1 >= AVERAGE_WEIGHT_MIN && weight1 <= AVERAGE_WEIGHT_MAX && volume1 <= AVERAGE_VOLUME_MAX}"
+            "${weight1 >= AVERAGE_WEIGHT_MIN && weight1 <= AVERAGE_WEIGHT_MAX && volume1 < AVERAGE_VOLUME_MAX}"
     )
 
     println(
         "Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average':" +
-                "${weight2 >= AVERAGE_WEIGHT_MIN && weight2 <= AVERAGE_WEIGHT_MAX && volume2 <= AVERAGE_VOLUME_MAX}"
+                "${weight2 >= AVERAGE_WEIGHT_MIN && weight2 <= AVERAGE_WEIGHT_MAX && volume2 < AVERAGE_VOLUME_MAX}"
     )
 }
 
 const val AVERAGE_WEIGHT_MIN = 36
 const val AVERAGE_WEIGHT_MAX = 100
-const val AVERAGE_VOLUME_MAX = 99
+const val AVERAGE_VOLUME_MAX = 100
