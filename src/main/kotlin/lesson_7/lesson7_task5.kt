0bc6d6539e4b36715921de71b996635525c5a41f
Anna Frolova <an.frolova023@gmail.com>
2024-11-27 19:47:04 +0700

    KS-7-5 strong password generator

---
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -0,0 +1,20 @@
+package lesson_7
+
+fun main() {
+
+    println("Введите число, которое будет соответсвовать количеству символов в пароле")
+    var charNumber = readln().toInt()
+    while (charNumber < MIN_LENGTH_OF_PASSWORD) {
+        println("Минимальное количество символов - 6")
+        charNumber = readln().toInt()
+    }
+
+    val rangesOfChars = (0..9) + ('a'..'z') + ('A'..'Z')
+
+    for (i in 1..charNumber) {
+        val randomChar = rangesOfChars.random()
+        print("$randomChar")
+    }
+}
+
+const val MIN_LENGTH_OF_PASSWORD = 6
