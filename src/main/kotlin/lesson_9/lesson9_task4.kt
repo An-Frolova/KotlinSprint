7095f75cf8390ecb9c148b5964ba28cde4ed8b63
Anna Frolova <an.frolova023@gmail.com>
2024-12-01 14:29:42 +0700

    KS-9-4 refactor

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -4,7 +4,6 @@ fun main() {
 
     println("Введите 5 ингредиентов через запятую")
     val input = readln()
-    val ingredients = input.split(", ").toMutableList()
-    val ingredientsSorted = ingredients.sorted()
-    println(ingredientsSorted)
+    val ingredients = input.split(", ").sorted()
+    println(ingredients)
 }
