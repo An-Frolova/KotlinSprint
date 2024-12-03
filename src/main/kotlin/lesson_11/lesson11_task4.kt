76e8e006813fd7cd62d310baf0a2f6b136b3d7ad
Anna Frolova <an.frolova023@gmail.com>
2024-12-03 20:56:43 +0700

    KS-11-4 Create entities for recipe app

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,25 @@
+package lesson_11
+
+
+class Category(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val imageUrl: String,
+)
+
+class Recipe(
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val category: Category,
+    val ingredients: MutableList<Ingredient>,
+    val cookingSteps: MutableList<String>,
+    var inFavorites: Boolean = false,
+)
+
+class Ingredient(
+    val id: Int,
+    val name: String,
+    val countForOneServing: Int,
+)
