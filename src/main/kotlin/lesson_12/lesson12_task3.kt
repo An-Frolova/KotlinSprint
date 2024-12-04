57c71dc7197c5c058d4fd15618e67778323dcd31
Anna Frolova <an.frolova023@gmail.com>
2024-12-04 16:10:12 +0700

    KS-12-3 Add Weather class with primary constructor 2

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,28 @@
+package lesson_12
+
+fun main() {
+    val weather = DailyWeather(300, 295, true)
+    weather.printInfo()
+}
+
+class DailyWeather(
+    dayTemperatureInKelvin: Int,
+    nightTemperatureInKelvin: Int,
+    isRainingInKelvin: Boolean
+) {
+    private val dayTemperatureInCelsius = (dayTemperatureInKelvin + KELVIN_ZERO).toInt()
+    private val nightTemperatureInCelsius = (nightTemperatureInKelvin + KELVIN_ZERO).toInt()
+    private val isRaining = isRainingInKelvin
+
+    fun printInfo() {
+        println(
+            """
+            Дневная температура: $dayTemperatureInCelsius
+            Ночная температура: $nightTemperatureInCelsius
+            Осадки в течение суток: ${if (isRaining) "да" else "нет"}
+        """.trimIndent()
+        )
+    }
+}
+
+const val KELVIN_ZERO = -273.15
