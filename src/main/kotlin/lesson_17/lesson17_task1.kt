1fb1c58fa93428ae7f541d93c15ab9f7a4983d87
Anna Frolova <an.frolova023@gmail.com>
2024-12-10 14:52:05 +0700

    KS-17-1 refactor

--- src/main/kotlin/lesson_17/lesson17_task1.kt
+++ src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -1,14 +1,14 @@
 package lesson_17
 
 class Quiz(
-    private val question: String,
-    private var answer: String,
+    val question: String,
+    var answer: String,
 ) {
 
-    val _question = question
+    private val _question = question
         get() = field
 
-    var _answer = answer
+    private var _answer = answer
         get() = field
         set(value) {
             field = value
