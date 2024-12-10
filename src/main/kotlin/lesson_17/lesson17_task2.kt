b305a1b3a686e8203dfd2674de2c5033f8bc7ded
Anna Frolova <an.frolova023@gmail.com>
2024-12-10 17:58:44 +0700

    KS-17-2 refactor: moved fields to primary constructor

--- src/main/kotlin/lesson_17/lesson17_task2.kt
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -2,17 +2,17 @@ package lesson_17
 
 fun main() {
 
-    val ship = Ship()
+    val ship = Ship("Надежда", 50, "Кейптаун")
     ship.name = "Черная жемчужина"
 }
 
-class Ship() {
+class Ship(
+    private var shipName: String,
+    private var speed: Int,
+    private var seaport: String,
+) {
 
-    private var _name = "Надежда"
-    private var _speed = 50
-    private var _seaport = "Кейптаун"
-
-    var name = _name
+    var name = shipName
         set(value) {
             println("Имя корабля изменить нельзя!")
         }
