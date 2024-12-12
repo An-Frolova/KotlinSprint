080f68e37d12158650b8da5862122bbbb2a06884
Anna Frolova <an.frolova023@gmail.com>
2024-12-12 15:30:19 +0700

    KS-19-2 add Category enum class for goods in online store

---
+++ src/main/kotlin/lesson_19/lesson19_task2.kt
@@ -0,0 +1,35 @@
+package lesson_19
+
+fun main() {
+
+    val item = Item("блокнот", 92, Category.STATIONERY)
+    val item2 = Item("шорты", 103, Category.CLOTHES)
+    val item3 = Item("заколка", 28, Category.OTHER)
+
+    item.printInfo()
+    item2.printInfo()
+    item3.printInfo()
+}
+
+enum class Category {
+
+    CLOTHES, STATIONERY, OTHER;
+
+    fun setCategoryName(category: Category): String {
+        return when (category) {
+            Category.CLOTHES -> "Одежда"
+            Category.STATIONERY -> "Канцелярские товары"
+            Category.OTHER -> "Разное"
+        }
+    }
+}
+
+class Item(
+    val name: String,
+    val id: Int,
+    val category: Category
+) {
+    fun printInfo() {
+        println("Название: $name Раздел: ${category.setCategoryName(category)}")
+    }
+}
