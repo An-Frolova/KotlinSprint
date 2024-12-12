4c1c8a72eb270a1a2fa87be508215b04d2b8246e
Anna Frolova <an.frolova023@gmail.com>
2024-12-12 13:16:23 +0700

    KS-18-2 refactor: add abstract variable facesCount

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -14,26 +14,24 @@ fun main() {
 
 abstract class Dice() {
 
-    open fun throwDice() {}
+    abstract val facesCount: Int
+
+    open fun throwDice() {
+        println((1..facesCount).random())
+    }
 }
 
 class Dice4Faces() : Dice() {
 
-    override fun throwDice() {
-        println((1..4).random())
-    }
+    override val facesCount: Int = 4
 }
 
 class Dice6Faces() : Dice() {
 
-    override fun throwDice() {
-        println((1..6).random())
-    }
+    override val facesCount: Int = 6
 }
 
 class Dice8Faces() : Dice() {
 
-    override fun throwDice() {
-        println((1..8).random())
-    }
+    override val facesCount: Int = 8
 }
