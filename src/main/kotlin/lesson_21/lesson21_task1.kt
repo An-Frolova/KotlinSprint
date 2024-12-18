2cd326ef78df3a50c1bb626a28239cbbddb6683c
Anna Frolova <an.frolova023@gmail.com>
2024-12-18 12:11:14 +0700

    KS-21-1 refactor

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -4,7 +4,5 @@ package lesson_21
 fun String.vowelCount(): Int {
 
     val vowels = listOf('a', 'e', 'i', 'o', 'u', 'y')
-    var count = 0
-    this.lowercase().forEach { if (it in vowels) count++ }
-    return count
+    return this.lowercase().count { it in vowels }
 }
